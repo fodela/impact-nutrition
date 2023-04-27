9758e0db574f4606652c2e11706ee69099ffc135
fodela <particleme30@gmail.com>
2023-04-27 10:59:55 +0000

    refactor homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <main className="main">
       <Hero />
-      <div>
+      <section>
         <h2 className="heading_secondary ">Insights and Analytics</h2>
         <div className="grid grid-cols-2 gap-10">
           <AnalyticContent />
@@ -16,7 +16,7 @@ export default async function Home() {
           <AnalyticContent />
           <AnalyticContent />
         </div>
-      </div>
+      </section>
 
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
