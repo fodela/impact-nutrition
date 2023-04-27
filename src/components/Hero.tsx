a238d6558c77f9deb5e4a7c15a1a2564efe01cbc
fodela <particleme30@gmail.com>
2023-04-27 11:01:00 +0000

    refactor header and hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <h1 className="heading_primary text-4xl font-bold uppercase mb-6">
           Impact nutrition consult leaders in nutrition for health
         </h1>
-        <p className="text-3xl">
+        <p className="text-3xl mb-4 ">
           We are committed to being leaders in the field of nutrition for
           optimal health. Our team of experienced and knowledgeable
           nutritionists are here to provide you with personalized guidance and
@@ -15,6 +15,7 @@ const Hero = () => {
           View more
         </button>
       </div>
+      <img src="./assets/hero_image.svg" alt="hero image" />
     </section>
   );
 };
