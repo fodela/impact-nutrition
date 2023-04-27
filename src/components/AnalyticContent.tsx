b1cd5262da695c52b5b6e583b9f4f9aef2570454
fodela <particleme30@gmail.com>
2023-04-27 11:01:47 +0000

    refactor analytic section

--- src/components/AnalyticContent.tsx
+++ src/components/AnalyticContent.tsx
@@ -12,7 +12,7 @@ const AnalyticContent = () => {
         />
       </div>
       <div>
-        <h5 className="font-bold text-3xl mb-4">Sed ut perspiciatis</h5>
+        <h5 className="heading_tertiary">Sed ut perspiciatis</h5>
         <p className="text-lg">
           Consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
           Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
