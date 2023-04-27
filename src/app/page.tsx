2fd51688344bbea35e7abc97b5dfcb87e5829a7f
fodela <particleme30@gmail.com>
2023-04-27 06:58:08 +0000

    add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import Hero from "@/components/Hero";
+import AnalyticContent from "@/components/AnalyticContent";
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="main">
+      <Hero />
+      <div>
+        <h2 className="heading_secondary ">Insights and Analytics</h2>
+        <div className="grid grid-cols-2 gap-10">
+          <AnalyticContent />
+          <AnalyticContent />
+          <AnalyticContent />
+          <AnalyticContent />
+        </div>
+      </div>
+
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -110,7 +122,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
-  )
+  );
 }
