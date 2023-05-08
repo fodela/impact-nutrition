eda268480bffc749b1485f08294935e03b22beb9
kelibst <kbooster17@gmail.com>
2023-05-08 18:43:57 +0000

    add active links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Logo from "./Logo";
 import NavBar from "./NavBar";
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,36 +1,33 @@
+'use client'
 import Link from "next/link";
 import SessionButtons from "./SessionButtons";
-
+import { useRouter } from "next/router";
+import { usePathname } from 'next/navigation';
 const NavBar = () => {
+  const pathname = usePathname();
+
+  // Define an array of link objects with href and label properties
+  const links = [
+    { href: '/', label: 'Home' },
+    { href: '/about', label: 'About' },
+    { href: '/services', label: 'Services' },
+    { href: '/blog', label: 'Blog' },
+    { href: '/contact_us', label: 'Contact Us' },
+  ];
+
   return (
     <ul className="flex gap-4">
-      <li>
-        <Link href={"/"} className="relative btn-effect">
-          Home
-          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
-        </Link>
+      {links.map(({ href, label }) => (
+        <li key={href}>
+          <Link href={href} legacyBehavior>
+            <a className={`relative btn-effect ${pathname === href ? 'active' : ''}`}>
+              {label}
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
+            </a>
+          </Link>
 
-      </li>
-      <li>
-        <Link href="/about" className="relative btn-effect"> About
-          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/services" className="relative btn-effect"> Services
-          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/blog" className="relative btn-effect"> Blog
-          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
-        </Link>
-      </li>
-      <li>
-        <Link href="/contact_us" className="relative btn-effect"> Contact us
-          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
-        </Link>
-      </li>
+        </li>
+      ))}
       <SessionButtons />
     </ul>
   );
