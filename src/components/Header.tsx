bfe55e0875f64574be6bcc2e5dfd8849a7cedc7a
fodela <particleme30@gmail.com>
2023-05-08 08:52:43 +0000

    refactor main header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-'use client'
+"use client";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <header className="flex max-w-screen-xl mx-auto justify-between items-center">
+    <header className="flex max-w-screen-xl mx-auto justify-between items-center px-6">
       <Logo />
       <NavBar />
     </header>
