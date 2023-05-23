c1959c65f6cf40fece7bfb8abaec8aa425530d81
kelibst <kbooster17@gmail.com>
2023-05-23 08:32:30 +0000

    rebase 1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -27,5 +27,20 @@ const Profile = async () => {
   );
 };
 
+<<<<<<< HEAD
+=======
+const Profile = (async () => {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        redirect('/api/auth/signin');
+    }
+    const { user } = session
+    //@ts-ignore
+    const role = user.role
+    return <div className="mx-2 rounded bg-gray-200">
+        <TabMenu role={role} />
+    </div>;
+});
+>>>>>>> 9cb1323 (add events and post api)
 
 export default Profile;
