43b37c5409807e237c6badc4d01120fb9548d71c
Keli Booster <kbooster17@gmail.com>
2023-05-23 10:05:07 +0000

    main (#14)

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 
 import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
-=======
-import { verifyUserRole } from "@/lib/verifyUserRole";
-import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
-import { authOptions } from "../../auth/[...nextauth]/route";
-import { NextApiRequest, NextApiResponse } from "next";
-import { validateAuthorization } from "../route";
-
->>>>>>> 9cb1323 (add events and post api)
 
 export async function GET(req: any, res: NextApiResponse) {
     const id = req.query.id! as string;
