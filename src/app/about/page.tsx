36ed99bec25bb7b062b77cb794bf20790581f958
fodela <particleme30@gmail.com>
2023-06-24 07:36:55 +0000

    refactor about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,36 +1,84 @@
 "use client";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 import { FaBullseye, FaEye, FaFlag } from "react-icons/fa";
 
 const about = () => {
   return (
     <main>
-      <section
-        className="my-24 mx-auto m-4 flex flex-col gap-12 max-w-screen-xl text-center text-xl
-      "
-      >
-        <h1 className="text-2xl md:text-4xl font-bold">Get to know more about us!</h1>
-        <article className="flex max-w-screen-lg mx-auto flex-col items-center">
-          <header className="text-center">
-            <FaBullseye size={100} />
-            <h6 className="text-4xl my-6 font-bold">Goal</h6>
-          </header>
-          <p className="text-xl max-w-screen-lg mx-auto md:text-2xl">
-            To provide a comprehensive and holistic counseling on food and
-            nutrition and their link with health in a sustainable manner to our
-            societies. We seek to research into and promote awareness on the
-            nutritional value of available local foods to our customers. To
-            empower the people we serve to be the doctors of the food they eat
-            and to use food as a weapon to boost immunity and to fight diseases.
+      <section className="grid md:grid-cols-2 max-w-screen-xl p-10 gap-10 mx-auto">
+        <article className="md:row-span-2 self-end text-colorPrimary text-8xl font-semibold revealFromLeft anim">
+          <p>Our</p>
+          <p>Mission</p>
+          <p>& Vision</p>
+        </article>
+        <Image
+          src="/assets/svg/about_us.svg"
+          width={604}
+          height={328}
+          alt="an illustration of people collaborating"
+        />
+        <article className="flex flex-col gap-4 ">
+          <h2 className="text-4xl text-colorPrimary font-bold revealFromRight ">
+            About Us
+          </h2>
+          <p className="revealFromDown ani-delay_1s">
+            Welcome to Impact Nutrition Consult, where we are committed to
+            combating the rise of non-communicable diseases through our
+            effective nutrition and diet-related services. As passionate agents
+            of nutrition promotion and advocacy, we empower individuals with the
+            knowledge and support they need to adopt and maintain good eating
+            practices. With a clear vision to establish a robust nutrition
+            education and counseling system in Ghana and create a global impact,
+            our goal is to provide comprehensive services that effectively
+            address the pressing challenges of our time.
           </p>
         </article>
-
-        <article className="flex max-w-screen-lg mx-auto flex-col items-center">
-          <header className="text-center">
-            <FaEye size={100} className="mx-auto" />
-            <h6 className="text-4xl my-6 font-bold">Vision</h6>
+      </section>
+      <section className="grid md:grid-cols-3 max-w-screen-xl mx-auto gap-10 p-10">
+        <article className="flex flex-col gap-4 justify-between p-6 border-2 border-colorPrimary rounded revealFromLeft ani-delay-500ms">
+          <header className="flex gap-2 items-center">
+            <Image
+              src="/assets/svg/mission.svg"
+              width={25}
+              height={25}
+              alt="an icon of mission"
+            />{" "}
+            <h3 className="text-4xl font-bold ">Mission</h3>
           </header>
-          <p className="text-xl max-w-screen-lg mx-auto md:text-2xl">
+          <p>
+            To provide a broad based nutrition and diets related services in an
+            effective way to combat the emergence of non-communicable diseases
+            in recent times. To be agent of nutrition promotion and advocacy for
+            good eating practices.
+          </p>
+        </article>
+        <article className="flex flex-col gap-4 justify-between p-6 border-2 border-colorPrimary rounded revealFromLeft ani-delay_1s">
+          <header className="flex gap-2 items-center">
+            <Image
+              src="/assets/svg/vision.svg"
+              width={25}
+              height={25}
+              alt="an icon of an eye"
+            />{" "}
+            <h3 className="text-4xl font-bold ">Vision</h3>
+          </header>
+          <p>
+            To establish a suitable and an effective nutrition education and
+            counseling system in Ghana and to be projected on the global map.
+          </p>
+        </article>
+        <article className="flex flex-col gap-4 justify-between p-6 border-2 border-colorPrimary rounded revealFromLeft ani-delay_1500ms">
+          <header className="flex gap-2 items-center">
+            <Image
+              src="/assets/svg/goal.svg"
+              width={25}
+              height={25}
+              alt="an icon of a target"
+            />{" "}
+            <h3 className="text-4xl font-bold ">Goal</h3>
+          </header>
+          <p>
             To provide a broad based nutrition and diets related services in an
             effective way to combat the emergence of non-communicable diseases
             in recent times. To be agent of nutrition promotion and advocacy for
@@ -38,55 +86,10 @@ const about = () => {
           </p>
         </article>
 
-        <article className="flex max-w-screen-lg mx-auto flex-col items-center">
-          <header className="text-center">
-            <FaFlag size={100} className="mx-auto" />
-            <h6 className="text-4xl my-6 font-bold">Mission</h6>
-          </header>
-          <p className="text-xl max-w-screen-lg mx-auto md:text-2xl">
-            To establish a suitable and an effective nutrition education and
-            counseling system in Ghana and to be projected on the global map.
-          </p>
-          <p className="text-xl max-w-screen-lg mx-auto md:text-2xl">
-            We anticipate that in the near future IMPACT NUTRITION CONSULT will
-            be placed on top of a pedestal as one of the leading nutrition
-            consulting mediums globally.
-          </p>
-        </article>
-        <figure className="md:flex mx-auto bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-          <img
-            className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-            src="assets/Saadong.jpg"
-            alt="executive director's image"
-            width="484"
-            height="612"
-          />
-          <div className="pt-6 mx-auto md:p-8 text-center md:text-left space-y-4">
-            <h3 className="text-2xl md:text-4xl font-bold">Executive Director&apos;s Message</h3>
-            <blockquote>
-              <p className="text-lg font-medium">
-                “As professionals we believe in our capabilities and we cannot
-                afford to disappoint our devotees. We have a strong conviction
-                in the route we have taken and we are confident we will
-                certainly get to our destination. We shall continue to work hard
-                in order to attain success. As a young NGO and with the capacity
-                of our team at the moment, we are confident there will be a lot
-                of successes moving into the future as we are determined to
-                shape the nutrition space of Ghana, Africa and beyond. Our
-                upcoming Continuous Professional Development (CPD) programs,
-                Community projects and our daily Nutrition tips on our social
-                media handles among others will be mind-blowing so you should
-                watch out for Impact Nutrition Consult.”
-              </p>
-            </blockquote>
-            <figcaption className="font-medium">
-              <div className="text-sky-500 dark:text-sky-400">Mr Executive</div>
-              <div className="text-slate-700 dark:text-slate-500">
-                Executive Director, INC
-              </div>
-            </figcaption>
-          </div>
-        </figure>
+        <dialog id="mine">
+          <p>I am in a dialog</p>
+        </dialog>
+        {/* <button onClick={() => mine.showModal()}></button> */}
       </section>
     </main>
   );
