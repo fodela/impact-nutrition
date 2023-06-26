56d113ba76c77b5617d22b4870b1862ed5dd6921
fodela <particleme30@gmail.com>
2023-06-26 08:20:10 +0000

    restyle navbar responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 import Link from "next/link";
 import SessionButtons from "./SessionButtons";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import { useRef, useState } from "react";
 import Sidebar from "./Sidebar";
 import { AiOutlineMenu } from "react-icons/ai";
@@ -9,15 +9,15 @@ const NavBar = () => {
   const pathname = usePathname();
   const sidebarRootRef = useRef<HTMLDivElement | null>(null);
 
-  // Define an array of link objects with href and label properties   
+  // Define an array of link objects with href and label properties
   // { href: '/services', label: 'Services' },
 
   const links = [
-    { href: '/', label: 'Home' },
-    { href: '/blog', label: 'Blogs' },
-    { href: '/events', label: 'Events' },
-    { href: '/about', label: 'About' },
-    { href: '/contact_us', label: 'Contact Us' },
+    { href: "/", label: "Home" },
+    { href: "/blog", label: "Blogs" },
+    { href: "/events", label: "Events" },
+    { href: "/about", label: "About Us" },
+    { href: "/contact_us", label: "Contact Us" },
   ];
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const toggleSidebar = () => {
@@ -25,22 +25,33 @@ const NavBar = () => {
   };
   return (
     <div>
-      <header className="flex justify-between sm:hidden">
-        <button className="pr-4 dark:text-white" onClick={toggleSidebar}><AiOutlineMenu className="dark:text-white" size={30} /></button>
+      <header className="flex justify-between md:hidden">
+        <button className="pr-4 dark:text-white" onClick={toggleSidebar}>
+          <AiOutlineMenu className="dark:text-white" size={30} />
+        </button>
       </header>
       <div ref={sidebarRootRef} id="sidebar-root" />
-      <Sidebar isOpen={isSidebarOpen} onClose={() => { toggleSidebar() }} sidebarRoot={sidebarRootRef.current} />
+      <Sidebar
+        isOpen={isSidebarOpen}
+        onClose={() => {
+          toggleSidebar();
+        }}
+        sidebarRoot={sidebarRootRef.current}
+      />
       <div className="hidden md:block">
-        <ul className="flex justify-center text-xl items-center gap-4">
+        <ul className="flex justify-center text-xl items-center gap-3">
           {links.map(({ href, label }) => (
             <li key={href}>
               <Link href={href} legacyBehavior>
-                <a className={`font-bold relative btn-effect ${pathname === href ? 'active' : ''}`}>
+                <a
+                  className={` relative btn-effect ${
+                    pathname === href ? "active text-colorPrimary" : ""
+                  }`}
+                >
                   {label}
                   <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
                 </a>
               </Link>
-
             </li>
           ))}
           <SessionButtons />
