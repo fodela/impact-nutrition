f312ac138bc14ccb8dafdb245681776f4ad9c5b5
fodela <particleme30@gmail.com>
2023-07-08 07:23:31 +0000

    resolve merge conflict

--- src/app/contact_us/page.tsx
+++ src/app/contact_us/page.tsx
@@ -10,18 +10,14 @@ const contact_us = () => {
         <h1 className="text-4xl font-bold text-center mt-20">
           Get in touch with Us
         </h1>
-<<<<<<< HEAD
-        <p className="mt-10 text-xl">
+
+        <p className="mt-10 mx-2 text-xl md:text-4xl">
           Our goal is to provide a comprehensive and holistic counseling on food
           and nutrition and their link with health in a sustainable manner to
           our societies. We seek to research into and promote awareness on the
           nutritional value of available local foods to our customers. To
           empower the people we serve to be the doctors of the food they eat and
           to use food as a weapon to boost immunity and to fight diseases.
-=======
-        <p className="mt-10 mx-2 text-xl md:text-4xl">
-          Our goal is to provide a comprehensive and holistic counseling on food and nutrition and their link with health in a sustainable manner to our societies. We seek to research into and promote awareness on the nutritional value of available local foods to our customers. To empower the people we serve to be the doctors of the food they eat and to use food as a weapon to boost immunity and to fight diseases.
->>>>>>> 1ed8918530cf62e89000e48e6809562a824deaf4
         </p>
         <div className="">
           <h4 className="text-2xl text-center my-5 font-bold">Follow us</h4>
