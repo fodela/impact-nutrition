a7c1ebbdac334d24702a59b8ab24c6c7120615e3
fodela <particleme30@gmail.com>
2023-10-16 04:23:19 +0000

    refactor user menu

--- src/components/SessionButtons.tsx
+++ src/components/SessionButtons.tsx
@@ -3,6 +3,8 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRef, useState } from "react";
 import { FiChevronDown, FiUser } from "react-icons/fi";
+import { LuLayoutPanelLeft } from "react-icons/lu";
+
 export default function SessionButtons() {
   const { data: session } = useSession();
   const [showAction, setShowAction] = useState(false);
@@ -20,8 +22,8 @@ export default function SessionButtons() {
           </span>
         </div>
         {showAction && (
-          <div className="absolute  w-48 mx-2 mt-3 right-6 rounded-xl shadow-md px-4 py-2 bg-white/80 dark:bg-black/80  revealFromTop">
-            <ul>
+          <div className="absolute flex flex-col justify-center items-center gap-4 w-48 mx-2 mt-3 p-8 right-6 rounded-xl shadow-md px-4 py-2 bg-white/80 dark:bg-white/10  revealFromTop">
+            <ul onClick={() => setShowAction(false)}>
               <li className="mb-1 font-bold relative">
                 <Link className="btn-effect" href="/dashboard">
                   {" "}
@@ -29,9 +31,20 @@ export default function SessionButtons() {
                   <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
                 </Link>
               </li>
+              <li>
+                <Link
+                  className="btn-effect flex items-center gap-2"
+                  href="/dashboard"
+                >
+                  {" "}
+                  <LuLayoutPanelLeft />
+                  Go to Dashboard
+                  <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 opacity-0"></span>
+                </Link>
+              </li>
             </ul>
             <button
-              className="bg-colorPrimary hover:bg-colorPrimary-200 px-4 py-1 ml-4 rounded text-white transition-colors duration-1200 "
+              className="bg-colorPrimary hover:bg-colorPrimary-200 px-4 py-1 rounded text-white transition-colors duration-1200 w-24"
               onClick={() => signOut()}
             >
               Sign out
