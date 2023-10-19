9ed233b1ca3c4d4ae9c7290de187e57e58857206
fodela <particleme30@gmail.com>
2023-10-19 08:38:39 +0000

    update dashboardcomponents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
             <GetPostsProvider>
               <GetEventsProvider>
                 <Header />
-                {/* <Hero /> */}
                 {children}
                 <Footer />
               </GetEventsProvider>
