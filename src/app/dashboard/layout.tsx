591688936a68bde9020d668a4f9c2d664e6dcd47
fodela <particleme30@gmail.com>
2023-10-19 13:12:44 +0000

    update dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,20 @@
 import { ToastContainer } from "react-toastify";
-import DashboardNav from "./DashboardNav";
+
 import "react-toastify/ReactToastify.min.css";
 import { NextAuthProvider } from "@/components/NextAuthProvider";
 export default function AboutLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return <NextAuthProvider>
-        <div className="pt-10 max-w-screen-xl md:mx-auto border-b border-gray-300">
-            <ToastContainer />
-            <DashboardNav />
-            <div className='dark:bg-black bg-white rounded-xl p-4 m-4'>
-                {children}
-            </div>
+  return (
+    <NextAuthProvider>
+      <div className="mt-10 pt-10 max-w-screen-xl md:mx-auto border-b border-gray-300 ">
+        <ToastContainer />
+        <div className="bg-[#EDF6EF] dark:bg-black rounded-xl p-4 m-4">
+          {children}
         </div>
+      </div>
     </NextAuthProvider>
-
+  );
 }
