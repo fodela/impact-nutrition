a6d7e6a00919c72f1ef880061c7fd57d1cdffb4a
kelibst <kbooster17@gmail.com>
2023-10-23 14:54:21 +0000

    update subscriber dashboard

--- src/app/dashboard/subscriber/page.tsx
+++ src/app/dashboard/subscriber/page.tsx
@@ -1,17 +1,12 @@
-'use client'
-import Pagination from "@/components/Dashboard/DashboardTable/Pagination";
-import TableHeader from "@/components/Dashboard/DashboardTable/TableHeader";
-import { GetAttendeesContext } from "@/components/context/AttendeeContext";
-import { GetEventContext } from "@/components/context/EventContext";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
-import { useContext, useEffect } from "react";
-import { BiTrashAlt } from "react-icons/bi";
-import { BsFillEyeFill } from "react-icons/bs";
-import { FaPencilAlt, FaUser } from "react-icons/fa";
+'use client';
+import { useContext, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { BsFillEyeFill } from 'react-icons/bs';
+import { GetEventContext } from '@/components/context/EventContext';
+import Pagination from '@/components/Dashboard/DashboardTable/Pagination';
 
-
-const headings = ["#", "Name", "Date Created", "Role", "Status", "actions"];
+const headings = ["#", "Title", "Details", "Location", "Price", "actions"];
 const summaries = [
     {
         label: "users",
@@ -33,60 +28,56 @@ const summaries = [
 
 const Subscriber = () => {
     const { myEvents, getAllMyEvents } = useContext(GetEventContext);
-    const {data: session} = useSession()
+    const { data: session } = useSession();
     console.log(myEvents, 'myevents')
+
     useEffect(() => {
-        if(session){
-            console.log(session,'session')
-            !myEvents.length && session.user &&  getAllMyEvents(session.user.id)
+        if (session) {
+            !myEvents && session.user && getAllMyEvents(session.user.id);
         }
-        // !myEvents && getAllMyEvents();
-        return () => { };
     }, [session]);
 
-    return <div className="flexx">
-        <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md my-5 divide-y-2 bg-gray-50 dark:bg-white/10">
-            {myEvents.length === 0 && <div>No events!</div>}
+    return (
+        <div className="subscription-container">
+            <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md my-5 divide-y-2 bg-gray-50 dark:bg-white/10">
+                {myEvents?.length === 0 && <div>No events!</div>}
 
-            <table className="w-full">
-                <thead className="p-4 m-4 text-dark bg-slate-300 rounded-xl border">
-                    <tr>
-                        <th>Title</th>
-                        <th>Details</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {myEvents.map(event => (
-                        <tr className="p-4 m-4" key={event.id}>
-                           
-                            <td>{event.title}</td>
-                            <td>
-                                <div dangerouslySetInnerHTML={{ __html: event.details }} />
-                            </td>
-                            <td>
-                                <div className="flex justify-end">
-                                    <Link className="" href={`/dashboard/admin/events/${event.id}`} legacyBehavior>
-                                        <a className=" font-bold"><BsFillEyeFill size={25} /> </a>
-                                    </Link>
-
-                                    {/* <button
-                                        onClick={() => handleDelete(event.id!)}
-                                        className="text-red-500  px-4 py-2 mr-2 rounded-md"
-                                    >
-                                        <BsTrash3 size={25} />
-                                    </button> */}
-                                </div>
-                            </td>
-
+                <table className="w-full">
+                    <thead>
+                        <tr className='p-4 m-4 rounded-xl border'>
+                            {headings.map((heading, index) => (
+                                <th className='px-2' key={index}>{heading}</th>
+                            ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <Pagination />
-
+                    </thead>
+                    <tbody>
+                        {myEvents?.map(({ id, title, details, location, price }, index) => (
+                            <tr className="p-4 m-4"  key={id}>
+                                <td className='p-4'>{index}</td>
+                                <td className='p-4'>{title}</td>
+                                <td className='p-4'>
+                                    <div dangerouslySetInnerHTML={{ __html: details }} />
+                                </td>
+                                <td className='p-4'>{location}</td>
+                                <td className='p-4'>{price}</td>
+                                <td className='p-4'>
+                                    <div className="flex justify-end">
+                                        <Link href={`/events/${id}`} legacyBehavior>
+                                            <a className="font-bold underline">
+                                                Open
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </td>
+                                
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <Pagination />
+            </div>
         </div>
-    </div>
+    );
 };
 
 export default Subscriber;
