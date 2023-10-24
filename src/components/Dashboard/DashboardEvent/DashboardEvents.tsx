a97e2edc7246a41636c5531c21bf039731de14bf
kelibst <kbooster17@gmail.com>
2023-10-24 11:37:02 +0000

    fix linters error

--- src/components/Dashboard/DashboardEvent/DashboardEvents.tsx
+++ src/components/Dashboard/DashboardEvent/DashboardEvents.tsx
@@ -1,123 +1,123 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-'use client'
-import { useState, useEffect, useRef, useTransition, useCallback, useMemo, useContext } from 'react';
-import 'suneditor/dist/css/suneditor.min.css';
-import { ToastContainer, toast } from 'react-toastify';
-import { Event } from '@prisma/client';
-import { deleteEvent } from '@/lib/getEvents';
-import UpdateEvent from './UpdateEvent';
-import { GetEventContext } from '@/components/context/EventContext';
-import Link from 'next/link';
+// /* eslint-disable react-hooks/exhaustive-deps */
+// 'use client'
+// import { useState, useEffect, useRef, useTransition, useCallback, useMemo, useContext } from 'react';
+// import 'suneditor/dist/css/suneditor.min.css';
+// import { ToastContainer, toast } from 'react-toastify';
+// import { Event } from '@prisma/client';
+// import { deleteEvent } from '@/lib/getEvents';
+// import UpdateEvent from './UpdateEvent';
+// import { GetEventContext } from '@/components/context/EventContext';
+// import Link from 'next/link';
 
-const DashboardEvents = () => {
-    const { events, getAllEvents } = useContext(GetEventContext);
+// const DashboardEvents = () => {
+//     const { events, getAllEvents } = useContext(GetEventContext);
 
-    const [updateEvent, setUpdateEvent] = useState(false);
-    const eventUpdateRef = useRef<HTMLDivElement | null>(null);
+//     const [updateEvent, setUpdateEvent] = useState(false);
+//     const eventUpdateRef = useRef<HTMLDivElement | null>(null);
 
-    const [startTransition, isPending] = useTransition();
+//     const [startTransition, isPending] = useTransition();
 
-    const toggleUpdateEvent = useCallback(() => {
-        setUpdateEvent(prevState => !prevState);
-    }, []);
+//     const toggleUpdateEvent = useCallback(() => {
+//         setUpdateEvent(prevState => !prevState);
+//     }, []);
 
-    const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
-    const memoizedSelectedEvent = useMemo(() => selectedEvent, [selectedEvent]);
+//     const [selectedEvent, setSelectedEvent] = useState<Event | null>(null);
+//     const memoizedSelectedEvent = useMemo(() => selectedEvent, [selectedEvent]);
 
-    useEffect(() => {
-        getAllEvents()
-    }, []);
+//     useEffect(() => {
+//         getAllEvents()
+//     }, []);
 
-    const handleDelete = useCallback(async (id: string) => {
-        const event = events.find(evt => evt.id === id);
-        setSelectedEvent(event!);
-        try {
-            await deleteEvent(event?.id!);
-            const notify = () => toast.success('Event successfully deleted!');
-            notify();
-        } catch (error) {
-            const notify = () =>
-                toast.error('Something went wrong!', {
-                    position: 'top-right',
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: 'colored',
-                });
-            notify();
-        }
-    }, [events]);
+//     const handleDelete = useCallback(async (id: string) => {
+//         const event = events.find(evt => evt.id === id);
+//         setSelectedEvent(event!);
+//         try {
+//             await deleteEvent(event?.id!);
+//             const notify = () => toast.success('Event successfully deleted!');
+//             notify();
+//         } catch (error) {
+//             const notify = () =>
+//                 toast.error('Something went wrong!', {
+//                     position: 'top-right',
+//                     autoClose: 5000,
+//                     hideProgressBar: false,
+//                     closeOnClick: true,
+//                     pauseOnHover: true,
+//                     draggable: true,
+//                     progress: undefined,
+//                     theme: 'colored',
+//                 });
+//             notify();
+//         }
+//     }, [events]);
 
-    const handleUpdate = useCallback((id: string) => {
-        const event = events.find(evt => evt.id === id);
-        if (event?.title) {
-            setSelectedEvent(event);
-            toggleUpdateEvent();
-        }
-    }, [events, toggleUpdateEvent]);
+//     const handleUpdate = useCallback((id: string) => {
+//         const event = events.find(evt => evt.id === id);
+//         if (event?.title) {
+//             setSelectedEvent(event);
+//             toggleUpdateEvent();
+//         }
+//     }, [events, toggleUpdateEvent]);
 
-    return (
-        <div className="p-4 max-w-screen-xl mx-auto">
-            <ToastContainer />
-            <div className="relative mb-2 flex justify-end">
-                <a href="/dashboard/events/addevent" className='p-3 rounded-lg bg-colorPrimary'> Add event</a>
-            </div>
-            {events.length === 0 && <div>No events!</div>}
-            <table className="w-full">
-                <thead className="p-4 m-4 text-dark bg-slate-300 rounded-xl border">
-                    <tr>
-                        <th>Title</th>
-                        <th>Details</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
+//     return (
+//         <div className="p-4 max-w-screen-xl mx-auto">
+//             <ToastContainer />
+//             <div className="relative mb-2 flex justify-end">
+//                 <a href="/dashboard/events/addevent" className='p-3 rounded-lg bg-colorPrimary'> Add event</a>
+//             </div>
+//             {events.length === 0 && <div>No events!</div>}
+//             <table className="w-full">
+//                 <thead className="p-4 m-4 text-dark bg-slate-300 rounded-xl border">
+//                     <tr>
+//                         <th>Title</th>
+//                         <th>Details</th>
+//                         <th>Actions</th>
+//                     </tr>
+//                 </thead>
 
-                <tbody>
-                    {events.map(event => (
+//                 <tbody>
+//                     {events.map(event => (
 
-                        <tr className="p-4 m-4" key={event.id}>
-                            <UpdateEvent
-                                isOpen={updateEvent}
-                                onClose={toggleUpdateEvent}
-                                //@ts-ignore
-                                event={memoizedSelectedEvent}
-                                //@ts-ignore
-                                eventUpdateRoot={eventUpdateRef}
-                            />
-                            <td>{event.title}</td>
-                            <td>
-                                <div dangerouslySetInnerHTML={{ __html: event.details }} />
-                            </td>
-                            <td>
-                                <div className="flex justify-end">
-                                    <Link className="btn_primary" href={`/dashboard/events/${event.id}`} legacyBehavior>
-                                        <a className="btn_primary font-bold">Open</a>
-                                    </Link>
-                                    <button
-                                        onClick={() => handleDelete(event.id!)}
-                                        className="text-red-500  px-4 py-2 mr-2 rounded-md"
-                                    >
-                                        Delete
-                                    </button>
-                                    <button
-                                        id={event.id}
-                                        onClick={() => handleUpdate(event.id!)}
-                                        className="text-blue-500 px-4 py-2 rounded-md"
-                                    >
-                                        Update
-                                    </button>
-                                </div>
-                            </td>
+//                         <tr className="p-4 m-4" key={event.id}>
+//                             {/* <UpdateEvent
+//                                 isOpen={updateEvent}
+//                                 onClose={toggleUpdateEvent}
+//                                 //@ts-ignore
+//                                 event={memoizedSelectedEvent}
+//                                 //@ts-ignore
+//                                 eventUpdateRoot={eventUpdateRef}
+//                             /> */}
+//                             <td>{event.title}</td>
+//                             <td>
+//                                 <div dangerouslySetInnerHTML={{ __html: event.details }} />
+//                             </td>
+//                             <td>
+//                                 <div className="flex justify-end">
+//                                     <Link className="btn_primary" href={`/dashboard/events/${event.id}`} legacyBehavior>
+//                                         <a className="btn_primary font-bold">Open</a>
+//                                     </Link>
+//                                     <button
+//                                         onClick={() => handleDelete(event.id!)}
+//                                         className="text-red-500  px-4 py-2 mr-2 rounded-md"
+//                                     >
+//                                         Delete
+//                                     </button>
+//                                     <button
+//                                         id={event.id}
+//                                         onClick={() => handleUpdate(event.id!)}
+//                                         className="text-blue-500 px-4 py-2 rounded-md"
+//                                     >
+//                                         Update
+//                                     </button>
+//                                 </div>
+//                             </td>
 
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-};
+//                         </tr>
+//                     ))}
+//                 </tbody>
+//             </table>
+//         </div>
+//     );
+// };
 
-export default DashboardEvents;
+// export default DashboardEvents;
--- src/components/Dashboard/DashboardTab.tsx
+++ src/components/Dashboard/DashboardTab.tsx
@@ -1,55 +1,55 @@
-'use client'
-import { verifyUserRole } from '@/lib/verifyUserRole';
-import React, { useState } from 'react';
-import DashboardContent from './DashboardContent/DashboardContent';
-import DashboardUser from './DashboardUser';
-import DashboardComments from './DashboardComments';
-import DashboardAction from './DashboarAction';
-import DashboardPost from './DashboardPost/DashboardPost';
-import DashboardEvents from './DashboardEvent/DashboardEvents';
+// 'use client'
+// import { verifyUserRole } from '@/lib/verifyUserRole';
+// import React, { useState } from 'react';
+// import DashboardContent from './DashboardContent/DashboardContent';
+// import DashboardUser from './DashboardUser';
+// import DashboardComments from './DashboardComments';
+// import DashboardAction from './DashboarAction';
+// import DashboardPost from './DashboardPost/DashboardPost';
+// import DashboardEvents from './DashboardEvent/DashboardEvents';
 
-interface props {
-    role: string
-}
+// interface props {
+//     role: string
+// }
 
 
 
-const TabMenu: React.FC<props> = ({ role }) => {
-    const [tabs] = useState(['Dashboard', 'Users', 'Posts', 'Comments', 'Events', 'Categories']);
-    const [activeTab, setActiveTab] = useState('Dashboard')
-    const onTabChange = (tab: string) => {
-        setActiveTab(tab);
-    }
-    return (
-        <div className="pt-10 max-w-screen-xl md:mx-auto border-b border-gray-300">
-            <nav className="flex justify-start">
-                {tabs.map((tab) => {
-                    if (verifyUserRole(role, tab) || tab === 'Dashboard')
-                        return (
-                            <button
-                                key={tab}
-                                onClick={() => onTabChange(tab)}
-                                className={`px-4 py-2 text-gray-900 font-medium hover:text-gray-200 hover:border-gray-200 focus:outline-none focus:text-gray-300 focus:border-gray-900 border-b-2 dark:text-white ${activeTab === tab ? 'border-gray-300' : 'border-transparent'
-                                    }`}
-                            >
-                                {tab}
-                            </button>
-                        )
-                })}
-            </nav>
-            <div className='dark:bg-black bg-white rounded-xl p-4 m-4'>
-                {activeTab !== 'Dashboard' && <DashboardAction tab={activeTab} />}
+// const TabMenu: React.FC<props> = ({ role }) => {
+//     const [tabs] = useState(['Dashboard', 'Users', 'Posts', 'Comments', 'Events', 'Categories']);
+//     const [activeTab, setActiveTab] = useState('Dashboard')
+//     const onTabChange = (tab: string) => {
+//         setActiveTab(tab);
+//     }
+//     return (
+//         <div className="pt-10 max-w-screen-xl md:mx-auto border-b border-gray-300">
+//             <nav className="flex justify-start">
+//                 {tabs.map((tab) => {
+//                     if (verifyUserRole(role, tab) || tab === 'Dashboard')
+//                         return (
+//                             <button
+//                                 key={tab}
+//                                 onClick={() => onTabChange(tab)}
+//                                 className={`px-4 py-2 text-gray-900 font-medium hover:text-gray-200 hover:border-gray-200 focus:outline-none focus:text-gray-300 focus:border-gray-900 border-b-2 dark:text-white ${activeTab === tab ? 'border-gray-300' : 'border-transparent'
+//                                     }`}
+//                             >
+//                                 {tab}
+//                             </button>
+//                         )
+//                 })}
+//             </nav>
+//             <div className='dark:bg-black bg-white rounded-xl p-4 m-4'>
+//                 {activeTab !== 'Dashboard' && <DashboardAction tab={activeTab} />}
 
-                {activeTab === 'Dashboard' && <DashboardContent />}
-                {activeTab === 'Users' && <DashboardUser />}
-                {activeTab === 'Posts' && <DashboardPost />}
-                {activeTab === 'Events' && <DashboardEvents />}
-                {activeTab === 'Categories' && <DashboardComments />}
-                {activeTab === 'Comments' && <DashboardComments />}
-            </div>
+//                 {activeTab === 'Dashboard' && <DashboardContent />}
+//                 {activeTab === 'Users' && <DashboardUser />}
+//                 {activeTab === 'Posts' && <DashboardPost />}
+//                 {activeTab === 'Events' && <DashboardEvents />}
+//                 {activeTab === 'Categories' && <DashboardComments />}
+//                 {activeTab === 'Comments' && <DashboardComments />}
+//             </div>
 
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default TabMenu;
+// export default TabMenu;
