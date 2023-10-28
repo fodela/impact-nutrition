2ebc824ee10a2c64b3eab6ec5eaa5bbafaf31697
fodela <particleme30@gmail.com>
2023-10-28 08:25:50 +0000

    refactor dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,5 @@
-'use client'
-import { ToastContainer } from "react-toastify";
+"use client";
 import "react-toastify/ReactToastify.min.css";
-import DashboardNav from "./DashboardNav";
 import { useSession } from "next-auth/react";
 
 export default function DashboardLayout({
@@ -11,24 +9,23 @@ export default function DashboardLayout({
 }) {
   const { status, data: session } = useSession({ required: true });
 
-
   if (status === "loading") {
     return <p>Loading...</p>;
   }
 
   if (session) {
-    const currentPath = window.location.href
+    const currentPath = window.location.href;
     const newPath =
-    //@ts-ignore
-      session.user.role === "SUBSCRIBER" && 
-      currentPath.includes('admin') ? 
-      currentPath.replace("/admin", "/subscriber") : 
       //@ts-ignore
-        session.user.role === "ADMIN" &&
-        currentPath.includes('subscriber') ? currentPath.replace("/subscriber", "/admin") : currentPath
+      session.user.role === "SUBSCRIBER" && currentPath.includes("admin")
+        ? currentPath.replace("/admin", "/subscriber")
+        : //@ts-ignore
+        session.user.role === "ADMIN" && currentPath.includes("subscriber")
+        ? currentPath.replace("/subscriber", "/admin")
+        : currentPath;
 
     if (currentPath !== newPath) {
-      window.location.href = (newPath);
+      window.location.href = newPath;
     }
   }
 
@@ -36,7 +33,6 @@ export default function DashboardLayout({
     <div>
       <div className="mt-10 pt-10 max-w-screen-2xl md:mx-auto border-b border-gray-300 ">
         <div className="bg-[#EDF6EF] dark:bg-black rounded-xl p-4 m-4">
-          <DashboardNav />
           {children}
         </div>
       </div>
