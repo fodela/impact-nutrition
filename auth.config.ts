db6b095047b3cda5f107a3694150c0fc6c49d079
Keli Booster <kbooster17@gmail.com>
2024-06-12 07:58:21 +0000

    refactor: use pnpm and update auth configurations

--- auth.config.ts
+++ auth.config.ts
@@ -1,85 +1,97 @@
-import type { NextAuthConfig } from "next-auth"
+import type { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Facebook from "next-auth/providers/facebook";
 import Google from "next-auth/providers/google";
 import Twitter from "next-auth/providers/twitter";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
-import Credentials from "next-auth/providers/credentials"
+import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
+import { signInSchema } from "@/lib/zod";
+import { ZodError } from "zod";
 
 export const authConfig = {
-    adapter: PrismaAdapter(prisma),
-
-    providers: [Google, Twitter, GitHub, Facebook,
-      Credentials({
-        name: "Phone",
-        credentials: {
-          phone: { label: "Phone", type: "text", placeholder: "0544249842" },
-          password: {
-            label: "Password",
-            type: "password",
-            placeholder: "Password",
-          },
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+    Google, Twitter, GitHub, Facebook,
+    Credentials({
+      name: "Phone",
+      credentials: {
+        phone: { label: "Phone", type: "text", placeholder: "0544249842" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
         },
-        authorize: async(credentials, req) => {
-          try {
-            if (!credentials?.phone || !credentials?.password) {
-              throw new Error("Please provide an email or a password");
-            }
-  
-            const user = await prisma.user.findUnique({
-              where: {
-                //@ts-ignore
-                phone: credentials?.phone,
-              },
-            });
-            if (!user) {
-              throw new Error("Wrong prhone number");
-            }
-            //@ts-ignore
-            const isPasswordValid = await compare(
-              //@ts-ignore
-              credentials?.password,
-              //@ts-ignore
-              user.password
-            );
-            console.log("ispasswordvalid", isPasswordValid)
-            if (!isPasswordValid) {
-              throw new Error("Wrong password");
-            }
-            return {
-              id: user.id,
-              phone: user.phone,
-              role: user.role,
-            };
-          } catch (error) {
-            console.log('error--errr', error)
+      },
+      authorize: async (credentials, req) => {
+        const { phone, password } = await signInSchema.parseAsync(credentials);
+        try {
+          const user = await prisma.user.findUnique({
+            where: {
+              phone: phone,
+            },
+          });
+          if (!user) {
+            throw new Error("Wrong phone number");
+          }
+//@ts-ignore
+          const isPasswordValid = await compare(password, user.password);
+          if (!isPasswordValid) {
+            throw new Error("Wrong password");
+          }
+
+          return {
+            id: user.id,
+            phone: user.phone,
+            name: user.name, // Ensure name is included
+            email: user.email, // Ensure email is included if available
+            image: user.image, // Ensure image is included if available
+            role: user.role,
+          };
+        } catch (error) {
+          if (error instanceof ZodError) {
             return null;
           }
-        },
-      }),
-    ],
-    pages: {
-      signIn: '/auth/signin',
-    }, 
-    callbacks: {
-      async session({session, user}) {
-        console.log(session, 'session')
-        session.user.id = user.id
-        return session
-      },
-       authorized({ auth, request: { nextUrl } }) {
-        const isLoggedIn = !!auth?.user
-        const paths = ["/user"]
-        const isProtected = paths.some((path) => nextUrl.pathname.startsWith(path))
-
-        if(isProtected && !isLoggedIn) {
-          const redirectUrl = new URL("/auth/login", nextUrl.origin)
-          redirectUrl.searchParams.append("callbackUrl", nextUrl.href)
-          return Response.redirect(redirectUrl)
+          return null;
         }
-        return true
-       }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user = {
+          ...session.user,
+          //@ts-ignore
+          id: token.id,
+          phone: token.phone,
+          name: token.name,
+          //@ts-ignore
+          email: token.email,
+          //@ts-ignore
+          image: token.image,
+          role: token.role,
+        };
+      }
+      return session;
     },
-} satisfies NextAuthConfig
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        //@ts-ignore
+        token.phone = user.phone;
+        token.name = user.name;
+        token.email = user.email;
+        token.image = user.image;
+        //@ts-ignore
+        token.role = user.role;
+      }
+      return token;
+    },
+  },
+} satisfies NextAuthConfig;
