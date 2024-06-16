b3936286f7f4edf79e67521506c3a8b8e36f1db7
fodela <particleme30@gmail.com>
2024-06-16 07:47:46 +0000

    style: fix primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        colorPrimary: "#5BAD34",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
