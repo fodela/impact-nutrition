30bb9531ca62ad2c7e86ac837411b75631ae93ce
fodela <particleme30@gmail.com>
2024-06-16 07:48:09 +0000

    style: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <div className="w-full fixed bg-current  p-4 top-0 left-0 z-20 shadow-md">
-      <header className="flex max-w-screen-2xl mx-auto justify-between items-center px-6 top-0">
+    <div className="w-full bg-background fixed p-4 top-0 left-0 z-20 shadow-md">
+      <header className="flex  max-w-screen-2xl mx-auto justify-between items-center px-6 top-0">
         <Logo />
         <NavBar />
       </header>
